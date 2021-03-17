c0c0be9bff5d897063a70409b2b498710d07380c
marcomasik <marcomasik@gmail.com>
2021-03-17 14:59:36 +0100

    Jokes have rating buttons

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -21,6 +21,7 @@ const Joke = () => {
     })
     .then((data) => { 
     	const jokeObject = data[0]
+    	jokeObject.funniness = 0
     	if (jokeList.some(existingJoke => existingJoke.id === jokeObject.id)) {
     		getJoke()
     		console.log("The joke already existed in jokeList, fetching again for a new random joke")
@@ -29,11 +30,31 @@ const Joke = () => {
     		setJokeList(prevArray => [...prevArray, jokeObject])
     		setIsLoading(false)
     		setShow(true)
+    		console.log(jokeList)
     	}
     })
     .catch(error => {
     	console.error('There has been a problem with the fetch operation:', error);
   	});
+  }
+
+  function increaseFunniness(id) {
+
+  	setJokeList(jokeList.map(jokes =>
+  		jokes.id === id
+  		? {...jokes, funniness : jokes.funniness + 1}
+  		: jokes
+  	))
+  	//jokeList.sort((a, b) => (a.funniness > b.funniness) ? 1 : -1)
+  }
+
+  function decreaseFunniness(id) {
+
+  	setJokeList(jokeList.map(jokes =>
+  		jokes.id === id
+  		? {...jokes, funniness : jokes.funniness - 1}
+  		: jokes
+  	))
   }
 
   //useEffect(() => getJoke(), [])
@@ -48,9 +69,15 @@ const Joke = () => {
 					{jokeList.map(joke => {
 						return(
 								<div className="main-container__jokelist-container__joke-container" key={joke.id}>
-									<h6>Joke Nr.{joke.id}</h6>
+									<div className="main-container__jokelist-container__joke-container__id-container">Joke Nr.{joke.id}</div>
 									<h3 className="main-container__jokelist-container__joke-container__setup-container">{joke.setup}</h3>
 									<h3 className="main-container__jokelist-container__joke-container__punchline-container">{joke.punchline}</h3>
+									<div className="main-container__jokelist-container__joke-container__funniness-container">
+										<button onClick={id => increaseFunniness(joke.id)} className="main-container__jokelist-container__joke-container__funniness-container__funniness-button main-container__jokelist-container__joke-container__funniness-container__funniness-button--plus">+</button>
+										<button onClick={id => decreaseFunniness(joke.id)} className="main-container__jokelist-container__joke-container__funniness-container__funniness-button main-container__jokelist-container__joke-container__funniness-container__funniness-button--minus">-</button>
+										<div className="main-container__jokelist-container__joke-container__funniness-container__funniness">Funniness: {joke.funniness}</div>
+									</div>
+									
 								</div>
 							)
 					})}
