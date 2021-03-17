bba4d462bcc9d1def3d24784d03bb4b87e3581a5
marcomasik <marcomasik@gmail.com>
2021-03-17 21:50:51 +0100

    Remove useEffect

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Joke = () => {
 
@@ -35,8 +35,6 @@ const Joke = () => {
     	console.error('There has been a problem with the fetch operation:', error);
   	});
   }
-
-  //useEffect(() => getJoke(), [])
 
 	return (
 		<>
